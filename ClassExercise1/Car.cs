c3b5fbc1b8e0ded1cd3203a69e7cf376ea94acb7
Ernest Acosta <ernestacosta@icloud.com>
2022-06-10 15:53:04 -0700

    initial commit

---
+++ ClassExercise1/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassExercise1
+{
+    internal class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+    }
+}
---
+++ ClassExercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            car.Make = "Ram";
+            car.Model = "TRX";
+            car.Year = "2023"; 
+
+            Console.WriteLine($"Car Make, Model, & Year");
+            Console.WriteLine($"-----------------------------------------");
+            {
+                Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+            }
+        }
+    }
+}
